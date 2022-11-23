0586cc4906e7a096150d49fff2bfef889e5f5205
Evan Duncan <evan.duncan@greenhouse.io>
2022-11-23 13:17:52 -0600

    Fix sql syntax

--- lib/active_record/timescale/schema_migration.rb
+++ lib/active_record/timescale/schema_migration.rb
@@ -47,7 +47,7 @@ module ActiveRecord
 
         create_table(relation, **hyper_table_options.args, &block) unless block.nil?
 
-        execute "SELECT create_hyper_table(#{relation}, #{time_column_name}, #{hyper_table_options.to_sql})"
+        execute "SELECT create_hyper_table('#{relation}', '#{time_column_name}', #{hyper_table_options.to_sql})"
       end
 
       # See create_hyper_table for examples. Works exactly the same but the distributed option is forced to TRUE
